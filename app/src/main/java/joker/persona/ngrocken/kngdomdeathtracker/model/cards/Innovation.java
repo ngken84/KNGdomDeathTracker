10abe120115571ba3f47db67239f8b8950cdba3d
Ken Ng <ng.ken84@gmail.com>
2018-10-12 21:54:48 -0700

    Add Innovation Fields and Innovation Construction Unit Test

--- app/src/main/java/joker/persona/ngrocken/kngdomdeathtracker/model/cards/Innovation.java
+++ app/src/main/java/joker/persona/ngrocken/kngdomdeathtracker/model/cards/Innovation.java
@@ -1,26 +1,52 @@
 package joker.persona.ngrocken.kngdomdeathtracker.model.cards;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import joker.persona.ngrocken.kngdomdeathtracker.model.effects.Effect;
 
 public class Innovation extends Card {
 
-    private String consequence;
+    private String category;
     private String consequenceOf;
     private int survivalLimitBonus;
     private List<Effect> effectList;
 
+    public Innovation(String name, String description, String category, String consequenceOf, int survivalLimitBonus) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.consequenceOf = consequenceOf;
+        this.survivalLimitBonus = survivalLimitBonus;
+    }
 
     @Override
     public CardType getCardType() {
         return CardType.INNOVATION;
     }
 
+    public String getConsequenceOf() {
+        return consequenceOf;
+    }
 
+    public String getCategory() {
+        return category;
+    }
 
+    public int getSurvivalLimitBonus() {
+        return survivalLimitBonus;
+    }
 
+    public List<Effect> getEffectList() {
+        return effectList;
+    }
 
+    public void addEffect(Effect effect) {
+        if(effectList == null) {
+            effectList = new LinkedList<>();
+        }
+        effectList.add(effect);
+    }
 
 
 
--- app/src/test/java/joker/persona/ngrocken/kngdomdeathtracker/InnovationCardUnitTest.java
+++ app/src/test/java/joker/persona/ngrocken/kngdomdeathtracker/InnovationCardUnitTest.java
@@ -10,9 +10,17 @@ import static org.junit.Assert.assertEquals;
 public class InnovationCardUnitTest {
 
     @Test
-    public void innovation_isInnovationType() {
-        Innovation testInnovation = new Innovation();
+    public void innovation_ConstructorWorks() {
+        String name = "Family";
+        String description = "";
+        String category = "home";
+        String consequenceOf = "hovel";
+        int slb = 0;
+
+        Innovation testInnovation = new Innovation(name, description, category, consequenceOf, slb);
         assertEquals(testInnovation.getCardType(), Card.CardType.INNOVATION);
+        assertEquals(testInnovation.getName(), name);
+        assertEquals(testInnovation.getCategory(), category);
     }
 
 }
