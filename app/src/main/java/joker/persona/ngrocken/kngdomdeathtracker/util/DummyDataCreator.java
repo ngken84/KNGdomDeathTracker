fa9540e9da13525c39cd8b8beef86e39f382e005
Ken Ng <ng.ken84@gmail.com>
2018-10-14 08:19:58 -0700

    Update Family Dummy Data

--- app/src/main/java/joker/persona/ngrocken/kngdomdeathtracker/util/DummyDataCreator.java
+++ app/src/main/java/joker/persona/ngrocken/kngdomdeathtracker/util/DummyDataCreator.java
@@ -9,7 +9,8 @@ public class DummyDataCreator {
 
     public static Innovation createForbiddenDanceInnovation() {
         Innovation card = new Innovation("Forbidden Dance", "", "music", "drums", 0);
-        Effect bonus = new Effect(Effect.GamePhase.SHOWDOWN, "When a survivor uses the <b>Synchronized Strike</b> secret fighting art, reroll missed attack rolls once.");
+        Effect bonus = new Effect(Effect.GamePhase.SHOWDOWN, "When a survivor uses the " +
+                "<b>Synchronized Strike</b> secret fighting art, reroll missed attack rolls once.");
         card.addEffect(bonus);
         EndeavorEvent event = new EndeavorEvent(1, "Once per lifetime roll 1d10");
         RollResult rollOne = new RollResult(1,5, "Ruined. Suffer -1 permanent movement.");
@@ -23,6 +24,11 @@ public class DummyDataCreator {
     public static Innovation createFamilyInnovation() {
         Innovation card = new Innovation("Family", "", "home", "hovel", 0);
         card.addEffect(createDepartingSurvivalEffect());
+        card.addEffect(new Effect(Effect.GamePhase.SETTLEMENT, "Survivors nominated for " +
+                "<b>intimacy</b> may give themselves a surname if they do not have one.<br>A newborn " +
+                "survivor inherits the surname of one parent, their weapon type, and half " +
+                "(rounded-down) of their weapon proficiency levels.", 0));
+
         return card;
     }
 
